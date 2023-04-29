61b2ff9ce6e8e3db34aaca976b151156b2f1e27b
Antoine <pollet.antoine.alexis@gmail.com>
2023-04-29 18:37:03 +0200

    Remove comment

--- api/src/Serializer/RoleContextBuilder.php
+++ api/src/Serializer/RoleContextBuilder.php
@@ -39,7 +39,7 @@ final class RoleContextBuilder implements SerializerContextBuilderInterface
                 if ($normalization) {
                     # Normalization part
                     if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
-                        // $context['groups'][] = 'admin:get';
+                        $context['groups'][] = 'admin:get';
                     } else if ($this->authorizationChecker->isGranted('ROLE_SUPER_VIP')) {
 
                     } else if ($this->authorizationChecker->isGranted('ROLE_VIP')) {
