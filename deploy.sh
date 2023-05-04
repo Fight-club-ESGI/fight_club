ccde15b146e37571936bdd46d089783756b778f8
Antoine lin <lin.antoine.pro@gmail.com>
2023-05-04 08:39:20 +0200

    fix cd

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+echo "Déploiement en cours..."
+
+# Change to the directory where the code is
+cd ~/fight_club/
+
+# Pull changes from the live branch
+git pull
+
+# Shut down the existing containers
+docker-compose down
+
+# Build the image with the new changes
+docker-compose build --no-cache
+
+# Start the new containers
+docker-compose up -d
+
+echo "C'est déployé, gg!"
