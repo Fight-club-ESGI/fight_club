010e2d4bbb614237cac9f2a935c6fef1516d34ac
Simon <simonmaltaire@gmail.com>
2023-05-11 08:34:25 +0200

    [misc] fix responsive for sm & md

--- front/src/views/bet/BetOnAFight.vue
+++ front/src/views/bet/BetOnAFight.vue
@@ -1,11 +1,11 @@
 <template>
     <v-container>
         <v-row no-gutters justify="space-around">
-            <v-col cols="12" sm="6">
+            <v-col cols="12" lg="6">
                 <BetOnAFightCard class="my-4" v-for="fight in fights" :fight="fight" />
             </v-col>
-            <v-col cols="12" sm="4">
-                <div class="text-center" v-if="$vuetify.display.name === 'xs'">
+            <v-col cols="12" lg="4">
+                <div class="text-center" v-if="$vuetify.display.name === 'xs' || $vuetify.display.name === 'sm' || $vuetify.display.name === 'md'">
                     <v-dialog v-model="showCurrentBetPreview" width="9/10" height="8/10">
                         <template v-slot:activator="{ props }">
                             <v-btn v-bind="props" size="80" rounded="xxl" icon="mdi-book-open" class="ma-4 fixed bottom-0 right-0"></v-btn>
@@ -15,7 +15,7 @@
                     </v-dialog>
                 </div>
                 <div v-else>
-                    <CurrentBetPreview class="mt-4 fixed top-18 right-18 w-120 h-8/10 overflow-y-auto" />
+                    <CurrentBetPreview class="mt-4 fixed top-18 right-18 w-110 h-8/10 overflow-y-auto" />
                 </div>
             </v-col>
         </v-row>
