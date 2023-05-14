8bbb6a7505b9af66f058754f53c55d53d04618dd
Maxime MARCHAND <mmarchand6@myges.fr>
2023-05-15 01:30:28 +0200

    Checkout Cart through Wallet

--- api/src/Controller/Cart/CartCheckout.php
+++ api/src/Controller/Cart/CartCheckout.php
@@ -4,9 +4,18 @@ namespace App\Controller\Cart;
 
 use App\Entity\Cart;
 use App\Entity\CartItem;
+use App\Entity\Order;
+use App\Entity\Ticket;
+use App\Entity\TicketEvent;
+use App\Entity\Wallet;
+use App\Entity\WalletTransaction;
+use App\Enum\WalletTransaction\WalletTransactionStatusEnum;
+use App\Enum\WalletTransaction\WalletTransactionTypeEnum;
 use App\Repository\CartRepository;
 use App\Service\Checkout\CheckoutService;
+use Doctrine\DBAL\LockMode;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
@@ -33,20 +42,108 @@ class CartCheckout extends AbstractController
             throw $this->createNotFoundException('Cart not found');
         }
 
-        $totalItems = $cart->getCartItems()->reduce(fn (int $carry, CartItem $item) => $carry + $item->getQuantity(), 0);
         $totalPrice = $cart->getCartItems()->reduce(fn (int $carry, CartItem $item) => $carry + ($item->getTicketEvent()->getPrice() * $item->getQuantity()), 0);
-
-        dd($totalItems, $totalPrice, $cart->getUser()->getWallet()->getAmount());
 
         switch ($type) {
             case 'wallet':
-                $this->checkoutService->checkoutWallet($cart);
+                $this->payWithWallet($cart, $totalPrice);
                 break;
             case 'stripe':
-                $this->checkoutService->checkoutStripe($cart);
+                $this->payWithStripe($cart, $totalPrice);
                 break;
         }
 
         return $cart;
+    }
+
+    private function payWithWallet(Cart $cart, int $totalPrice): void
+    {
+        $user = $this->security->getUser();
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $wallet = $user->getWallet();
+
+        if (!$wallet) {
+            throw $this->createNotFoundException('Wallet not found');
+        }
+
+        if ($wallet->getAmount() < $totalPrice) {
+            throw $this->createNotFoundException('Not enough money');
+        }
+
+        $order = (new Order())
+            ->setUser($user)
+            ->setPrice($totalPrice);
+
+        $this->entityManager->persist($order);
+
+        $walletTransaction = $this->checkoutService->recordWalletTransaction($wallet, $totalPrice, WalletTransactionStatusEnum::PENDING, WalletTransactionTypeEnum::WALLET_PAYMENT);
+        $this->checkoutService->payment($walletTransaction);
+
+        [$createdTickets, $refundTickets] = $this->generateTickets($order, $cart->getCartItems());
+
+        $this->entityManager->flush();
+
+        if (count($refundTickets) > 0) {
+            $refundPrice = array_reduce($refundTickets, fn (int $carry, $item) => $carry + $item['ticketEvent']->getPrice() * $item['quantity'], 0);
+
+            $refundWalletTransaction = (new WalletTransaction)
+                ->setWallet($wallet);
+
+            $this->checkoutService->refund($refundWalletTransaction, $refundPrice);
+        }
+    }
+
+    private function generateTickets($order, $cartItems)
+    {
+        $tickets = [];
+        $refundTickets = [];
+
+        $this->entityManager->getConnection()->beginTransaction();
+
+        try {
+
+            foreach ($cartItems as $cartItem) {
+                $remainingQuantity = $cartItem->getQuantity();
+                $ticketEventId = $cartItem->getTicketEvent()->getId();
+
+                while ($remainingQuantity > 0) {
+                    $ticketEvent = $this->entityManager->find(TicketEvent::class, $ticketEventId, LockMode::PESSIMISTIC_WRITE);
+
+                    if ($ticketEvent->getMaxQuantity() > $ticketEvent->getTickets()->count()) {
+                        $ticket = (new Ticket())
+                            ->setTicketEvent($cartItem->getTicketEvent())
+                            ->setOrder($order);
+
+                        $this->entityManager->persist($ticket);
+                        $this->entityManager->flush();
+                        $tickets[] = $ticket;
+                        $remainingQuantity--;
+                    } else {
+                        break;
+                    }
+                }
+
+                if ($remainingQuantity > 0) {
+                    $refundTickets[] = [
+                        'ticketEvent' => $cartItem->getTicketEvent(),
+                        'quantity' => $remainingQuantity,
+                    ];
+                }
+
+                $this->entityManager->remove($cartItem);
+            }
+
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+
+            return [$tickets, $refundTickets];
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
     }
 }
