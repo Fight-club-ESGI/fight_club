f3a1c887dcb0a9045296fe52c3b4f0e37159d6b1
Maxime MARCHAND <mmarchand6@myges.fr>
2023-05-21 15:37:39 +0200

    Fix conflicts

--- front/src/components/ticket/TicketEventCard.vue
+++ front/src/components/ticket/TicketEventCard.vue
@@ -21,7 +21,7 @@ const props = defineProps({
     }
 })
 
-const canAddToCart = computed(() => {
+const cartQuantity = computed(() => {
 
     let cartQuantity = cart.value?.cartItems.reduce((acc, item) => {
         if (item.ticketEvent && item.ticketEvent.id == props.ticketEvent.id) {
@@ -30,30 +30,28 @@ const canAddToCart = computed(() => {
         return acc;
     }, 0) || 0;
 
-    if (props.ticketEvent.maxQuantity - props.ticketEvent.tickets.length - cartQuantity < quantity.value)
+    return cartQuantity;
+
+});
+
+const canAddToCart = computed(() => {
+
+    if (maxCanAddToCart.value < quantity.value)
         return false;
 
     return true;
+
+});
+
+const maxCanAddToCart = computed(() => {
+
+    return props.ticketEvent.maxQuantity - props.ticketEvent.tickets.length - cartQuantity.value;
+
 });
 
 const addCart = async (ticketEvent: string) => {
 
-    if (!canAddToCart) {
-        createToast('Not enough tickets available', {
-            type: 'danger',
-            position: 'bottom-right'
-        });
-        return;
-    }
-
-    let cartQuantity = cart.value?.cartItems.reduce((acc, item) => {
-        if (item.ticketEvent && item.ticketEvent.id == ticketEvent) {
-            return acc + item.quantity;
-        }
-        return acc;
-    }, 0) || 0;
-
-    if (props.ticketEvent.maxQuantity - props.ticketEvent.tickets.length - cartQuantity < quantity.value) {
+    if (!canAddToCart.value) {
         createToast('Not enough tickets available', {
             type: 'danger',
             position: 'bottom-right'
@@ -79,17 +77,7 @@ const addCart = async (ticketEvent: string) => {
 }
 
 const checkNumber = () => {
-    quantity.value = Math.min(maxCanAddToCart.value, Math.max(1, Number(quantity.value))) || 1;
-}
-
-const increment = () => {
-    quantity.value++;
-    checkNumber();
-}
-
-const decrement = () => {
-    quantity.value--;
-    checkNumber();
+    quantity.value = Math.min(maxCanAddToCart.value, Math.max(1, Number(quantity.value)));
 }
 </script>
 
@@ -111,9 +99,8 @@ const decrement = () => {
             <div v-else>
                 <span class="font-bold">Available : </span>
                 <span>{{ props.ticketEvent.maxQuantity - props.ticketEvent.tickets.length }} / {{
-                    props.ticketEvent.maxQuantity }}
-                    <span v-if="cartQuantity > 0">( {{ cartQuantity }} in your cart )</span>
-                </span>
+                    props.ticketEvent.maxQuantity
+                }} <span v-if="cartQuantity > 0">( {{ cartQuantity }} in your cart )</span></span>
             </div>
         </v-card-text>
         <div v-if="new Date() <= new Date(ticketEvent.event.timeEnd) && isConnected">
