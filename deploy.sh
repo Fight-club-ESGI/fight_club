3cf95e2547a91e5c4df555c8ad586f550462c9ee
Antoine lin <lin.antoine.pro@gmail.com>
2023-05-22 12:50:21 +0200

    update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+#docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+docker run -d -p 80:80 --name fight_club-latest tufeng/fight_club:latest
