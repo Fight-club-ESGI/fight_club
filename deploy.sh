63f368ba679f4aeecbd785ea39108491ef1ce0b1
Antoine lin <lin.antoine.pro@gmail.com>
2023-05-24 21:54:42 +0200

    add migration file

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 #docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 #docker run -d -p 80:80 --name fight_club-latest tufeng/fight_club:latest
 #docker run -d -p 80:80 --name fight_club-latest tufeng/fight_club:latest
-git clone https://github.com/Fight-club-ESGI/fight_club.git
-cd ./fight_club
-docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
+#git clone https://github.com/Fight-club-ESGI/fight_club.git
+#cd ./fight_club
+#docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
