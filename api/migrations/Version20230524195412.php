7af506ac77d76c3a5393413c170d0b148e695145
Antoine lin <lin.antoine.pro@gmail.com>
2023-05-27 23:11:13 +0200

    fix migration

--- api/migrations/Version20230524195412.php
+++ api/migrations/Version20230524195412.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230527123855 extends AbstractMigration
+final class Version20230524195412 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,13 @@ final class Version20230527123855 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('ALTER TABLE fighter ADD is_active BOOLEAN NOT NULL DEFAULT FALSE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE fighter DROP is_active');
     }
 }
